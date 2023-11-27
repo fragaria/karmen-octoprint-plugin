9b504854a3f3feb9f36078f3680a9f31fd7aacba
Robin Gottfried <robin.gottfried@fragaria.cz>
2023-11-27 15:52:01 +0100

    [cleanup/fix] sentry error capturing, fixed error on connection

--- octoprint_karmen/utils/__init__.py
+++ octoprint_karmen/utils/__init__.py
@@ -1,5 +1,17 @@
+import time
 from .sentry import SentryWrapper
 from .get_my_ip import get_ip
 
+
 def parse_path_whitelist(path_whitelist_settings: str) -> tuple:
     return tuple(filter(None, path_whitelist_settings.split(';')))
+
+def wait_till_true(condition: callable, max_timeout=0.5):
+    begin = time.monotonic()
+    result = False
+    while not result and time.monotonic() - begin <= max_timeout:
+        result = condition()
+        if result:
+            break
+        time.sleep(0.1)
+    return result
